ab5be6e7ff908f8f295bdf48827820fb9597445d
ryukyeongseo <113505062+ryukyeongseo@users.noreply.github.com>
2023-03-22 17:02:02 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let answer =0;
+  let str = n.toString();
+    let arr = str.split("");
+  for(let i=0; i<arr.length; i++){
+      answer += Number(arr[i])
+  }
+return answer
+}
