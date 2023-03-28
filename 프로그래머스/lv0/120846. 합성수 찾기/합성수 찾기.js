3075834afb7cd2fb79f30a171f79dd40b825ddda
ryukyeongseo <113505062+ryukyeongseo@users.noreply.github.com>
2023-03-28 21:35:03 +0900

    [level 0] Title: 합성수 찾기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    for(let i=1; i<=n; i++){
+        let count =0;
+        for(let j=0; j<=i; j++){
+            if(i%j===0)count++
+        }
+        if(count>2){
+            answer +=1
+        }
+    }
+    return answer;
+}
