6f315056c59f5cf052ca22afc6094963141ecb4d
Ryu i seo <113505062+ryuiseo@users.noreply.github.com>
2023-04-10 23:30:18 +0900

    [level 0] Title: 로그인 성공?, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,13 @@
+function solution(id_pw, db) {
+    var answer = '';
+    for(let i=0; i<db.length; i++){
+        if(id_pw[0]===db[i][0] && id_pw[1]===db[i][1]){
+            answer = 'login'
+        }else if(id_pw[0]===db[i][0] && id_pw[1]!==db[i][1]){
+            answer ='wrong pw'
+        }else if(id_pw[0]!==db[i][0] && id_pw[1]!==db[i][1]){
+            answer ='fail'
+        }
+    }
+    return answer;
+}
