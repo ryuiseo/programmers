9f8a3ff0a05ddb6615ca259f299de8638403439a
Ryu i seo <113505062+ryuiseo@users.noreply.github.com>
2023-09-04 22:46:00 +0900

    [unrated] Title: l로 만들기, Time: 7.99 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -0,0 +1,12 @@
+function solution(myString) {
+    var answer = '';
+    
+    for(let i=0; i<myString.length; i++){
+        if(myString[i] < 'l'){
+            answer += "l"
+        }else {
+            answer += myString[i];
+        }
+    }
+    return answer;
+}
