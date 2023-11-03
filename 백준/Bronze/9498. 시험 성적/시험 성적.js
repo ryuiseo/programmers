4b44529b623f347d8ef02414fbb12f80e451ff85
Ryu i seo <113505062+ryuiseo@users.noreply.github.com>
2023-11-03 20:08:47 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let a = parseInt(input[0]);
+
+if(90 <= a && a<=100){
+     console.log('A')
+}else if(80<=a && a <=89){
+    console.log('B')
+}else if(70<=a && a<=79){
+     console.log('C')
+}else if(60<=a && a<=69){
+     console.log('D')
+}else {
+     console.log('F')
+};
