a69998b6519750d461f53ef163641529c1c6a227
Ryu i seo <113505062+ryuiseo@users.noreply.github.com>
2023-11-04 16:23:10 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,9 +1,8 @@
 let fs = require('fs');
-let input =fs.readFileSync('/dev/stdin').toString().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let data = input.map(Number);
 let mySet = new Set();
-
 for(let i=0; i<10; i++){
     mySet.add(data[i]%42);
 }
