e7e11bda4db5ae38262bbcd24ec21f9f0f84e042
Ryu i seo <113505062+ryuiseo@users.noreply.github.com>
2023-11-04 17:22:33 +0900

    [Bronze V] Title: 윤년, Time: 136 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let year = Number(input[0]);
+
+if(year%4 === 0 && year%100 !==0 || year%400===0 ){
+    console.log('1')
+}else {
+    console.log('0')
+}
