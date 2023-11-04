da8044609c7b870652fa43e8c8029e40b81b8364
Ryu i seo <113505062+ryuiseo@users.noreply.github.com>
2023-11-04 15:35:37 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1348 ms, Memory: 164484 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let num = Number(input[0]);
+let answer = '';
+
+for(let i=1; i<=num; i++){
+    let data = input[i].split(' ');
+    let a = Number(data[0]);
+    let b = Number(data[1]);
+    
+    answer += a + b + "\n"
+}
+
+console.log(answer);
